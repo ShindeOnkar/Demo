eb0456fbe4449280c8042ad09886a1be56997e64
onkar shinde <you@example.com>
2022-02-18 20:52:14 +0530

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { UserDataService } from './user-data.service';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule 
+   
+  ],
+  providers: [UserDataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
